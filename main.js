5b7084a7d6878a6086a1061c794f4c5c5ba724cc
Prakhar71008 <80336234+Prakhar71008@users.noreply.github.com>
2022-06-16 20:45:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(640,480);
+canvas.position(350,350);
+video=createCapture(VIDEO);
+video.hide();
+
+tint_colour="";
+}
+ function draw(){
+image(video, 230, 150, 220, 200);
+tint(tint_colour);
+
+fill(255,0,0);
+stroke(255,0,0);
+circle(50,50,80);
+
+circle(590,50,80);
+circle(50,430,80);
+circle(590,430,80);
+
+fill(0,255,0);
+stroke(0,255,0);
+rect(90,40,460,20);
+rect(90,420,460,20);
+rect(40,90,20,300);
+rect(580,90,20,300);
+
+ }
+
+ function take_snapshot(){
+save("Filter_image.png");
+ }
+
+ function Apply_filter(){
+tint_colour=document.getElementById("name_colour").value;
+ }
